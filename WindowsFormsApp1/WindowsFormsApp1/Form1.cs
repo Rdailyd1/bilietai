b8065107438f939cd6e800da5a018827fe8120fb
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-10 21:43:00 +0300

    Bilietai po 10

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int bilietai10;
+            public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
